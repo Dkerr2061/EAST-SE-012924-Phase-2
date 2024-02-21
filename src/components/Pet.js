b3fb8c2aae1f332514c3b911ed4f25ba4d04a5f0
David Kerr <kerrky@gmail.com>
2024-02-21 11:52:03 -0500

    Updated Lecture Code

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 function Pet({pet}){
+    const [ count, setCount ] = useState(0)
+    const [ displayAnimalType, setDisplayAnimalType ] = useState(false)
+
+    const animalClassName = displayAnimalType ? "display-animal-type" : ""
+    
+
+    function handleClick() {
+        setCount((count) => count + 1)
+    }
+
+    function toggleSwitch() {
+        setDisplayAnimalType((displayAnimalType) => !displayAnimalType)
+    }
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
-            <h4 className="">{pet.name}</h4>
-            <button className="like-button">0 Likes</button>
+            <h4 
+                className={animalClassName} 
+                onClick={toggleSwitch}>
+                {displayAnimalType ? pet.animal_type : pet.name}
+            </h4>
+            <button 
+                className="like-button" 
+                onClick={handleClick}>
+                {`${count} Likes`}
+            </button>
         </li>
     );
 }
--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,9 +1,19 @@
 import PetList from "./PetList";
 import pets from "../data/pets";
+import { useState } from "react";
 
 console.log(pets)
 
 function PetPage(){
+const [ filterBy, setFilterBy ] = useState("")
+
+// Rememeber that you can stack methods and use them one after another. This is useful to streamline your code and make it easier to check for conditions instead of doing long if/else statements. This is good practice! Learn this!
+
+const filteredPets = pets.filter((pet) => {
+  return pet.name.toUpperCase().includes(filterBy.toUpperCase()) 
+})
+
+
     return (
         <main>
             <div className="searchbar">
@@ -12,9 +22,10 @@ function PetPage(){
                     type="text"
                     id="search"
                     placeholder="Type a name to search..."
+                    onChange={(event) =>  setFilterBy(event.target.value) }
                 />
             </div>
-            <PetList pets={pets}/>
+            <PetList pets={filteredPets}/>
         </main>
     );
 }
